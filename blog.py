97b83cc5d8090f444eb42d7a0135266e7195e73d
Eric Hambro <eric.hambro@gmail.com>
2014-08-22 14:43:37 +0100

    added sessions and connect to SQL db

--- blog.py
+++ blog.py
@@ -3,9 +3,11 @@
 
 
 # imports
+from functools import wraps
 from flask import Flask, render_template, request, session, \
                 flash, redirect, url_for, g
 import sqlite3
+
 
 
 # configurations
@@ -23,6 +25,16 @@ app.config.from_object(__name__)
 #write a function for connecting to database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to log in first.")
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -45,11 +57,18 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('login'))    
+    return redirect(url_for('login')) 
 
+   
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts = posts)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
